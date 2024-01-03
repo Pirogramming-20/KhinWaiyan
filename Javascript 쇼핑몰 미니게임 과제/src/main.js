8a0e545399cbf0a78404fab6054c7395c193dcbd
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-03 14:08:26 +0900

    5. JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,12 +5,30 @@ function loadItems() {
     .then(json => json.items)
 }
 
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    // update innerHTML
+    // const html = items.map(item => createHTMLString(item)).join('');
+    // console.log(html);
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+    // join('') : array를 string으로 변환
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 
 //main
 loadItems()
     .then(items => {
-        console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items);
     })
     .catch(console.log);
