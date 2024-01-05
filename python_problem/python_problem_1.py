a9a78689f9d20ddc15004d6c9d043a9d2673df18
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-05 16:40:38 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,10 +1,6 @@
-num = 0
-playerList = ["playerA", "playerB"]
-curr_player = 0
-
-while num < 31:
+def brGame(player, num):
     while True:
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+        user_input = input(f"{player}, 부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
         try:
             user_input = int(user_input)
         except ValueError:
@@ -14,11 +10,21 @@ while num < 31:
             break
         else:
             print("1,2,3 중 하나를 입력하세요")
-
+    
     for i in range(user_input):
         num += 1
-        print(f"{playerList[curr_player]} : {num}")
+        print(f"{player} : {num}")
         if num >= 31:
-            print(f"{playerList[(curr_player + 1) % 2]} win!")
-            break
+            return num
+    return num
+
+num = 0
+playerList = ["playerA", "playerB"]
+curr_player = 0
+
+while num < 31:
+    num = brGame(playerList[curr_player], num)
+    if num >= 31:
+        print(f"{playerList[(curr_player + 1) % 2]} win!")
+        break
     curr_player = (curr_player + 1) % 2
