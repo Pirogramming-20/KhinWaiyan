f55d55c9beaa3347c18c445432fbd89c4ad8417d
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-05 16:37:50 +0900

    1-7

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,9 +1,8 @@
 num = 0
-player_names = ["playerA", "playerB"]
-current_player = 0
+playerList = ["playerA", "playerB"]
+curr_player = 0
 
 while num < 31:
-
     while True:
         user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
         try:
@@ -18,7 +17,8 @@ while num < 31:
 
     for i in range(user_input):
         num += 1
-        print(f"{player_names[current_player]} : {num}")
+        print(f"{playerList[curr_player]} : {num}")
         if num >= 31:
+            print(f"{playerList[(curr_player + 1) % 2]} win!")
             break
-    current_player = (current_player + 1) % 2
+    curr_player = (curr_player + 1) % 2
