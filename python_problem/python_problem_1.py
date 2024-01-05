4c262c2122dabbbfcbd4d98ef7608d96479eb252
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-05 16:32:31 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,35 +1,24 @@
 num = 0
-player_name = "playerA"
-next_player = "playerB"
+player_names = ["playerA", "playerB"]
+current_player = 0
 
-while True:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-    try:
-        user_input = int(user_input)
-    except ValueError:
-        print("정수를 입력하세요.")
-        continue
-    if user_input in [1, 2, 3]:
-        break
-    else:
-        print("1,2,3 중 하나를 입력하세요")
+while num < 31:
 
-for i in range(user_input):
-    num += 1
-    print(f"{player_name} : {num}")
+    while True:
+        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print("정수를 입력하세요.")
+            continue
+        if user_input in [1, 2, 3]:
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")
 
-while True:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-    try:
-        user_input = int(user_input)
-    except ValueError:
-        print("정수를 입력하세요.")
-        continue
-    if user_input in [1, 2, 3]:
-        break
-    else:
-        print("1,2,3 중 하나를 입력하세요")
-
-for i in range(user_input):
-    num += 1
-    print(f"{next_player} : {num}")
+    for i in range(user_input):
+        num += 1
+        print(f"{player_names[current_player]} : {num}")
+        if num >= 31:
+            break
+    current_player = (current_player + 1) % 2
