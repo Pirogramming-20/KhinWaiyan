6697a4d5665b9b980b6160aaf5bffc4d24801e0a
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-10 23:42:56 +0900

    애플리케이션 확장하기

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post # . means current directory or current application
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # filter posts that are published
     return render(request, 'blog/post_list.html', {'posts': posts}) # show post_list.html template #'posts': posts is parameter to be passed to template
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # get post with pk=pk or return 404 error
+    return render(request, 'blog/post_detail.html', {'post': post}) # show post_list.html template #'posts': posts is parameter to be passed to template
