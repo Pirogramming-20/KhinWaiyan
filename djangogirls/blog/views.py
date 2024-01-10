0eaa29e2736b894940bb4bc7d9055b82fb747c52
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-11 00:05:03 +0900

    Django 폼

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post # . means current directory or current application
 from django.utils import timezone
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +12,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk) # get post with pk=pk or return 404 error
     return render(request, 'blog/post_detail.html', {'post': post}) # show post_list.html template #'posts': posts is parameter to be passed to template
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
